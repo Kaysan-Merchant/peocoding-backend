1d66089a7bdb8453cace9dc217430f99da8291ff
Kaysan Merchant <merch.kk12@gmail.com>
2025-10-15 19:37:02 -0400

    added example endpoint

--- api/main.py
+++ api/main.py
@@ -8,10 +8,16 @@ app = FastAPI(
 )
 
 # Define endpoints or routes(PATH)
-@app.get('/') # a "/" on its own is a root directory, sort of like a menu
+@app.get('/') # a "/" on its own is the root directory
 def default_route():
     """
     This is the default endpoint for this back-end.
     """
     return "You have reached the default route. Back-end server is listening..."
 
+@app.get("/example")  
+def get_example():    
+    """
+    This endpoint returns a JSON object consisting of a simple message.
+    """
+    return {"message": "Hello World!"}
