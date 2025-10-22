c94962e94c16df4ec625323bf8f49443224945b5
Kaysan Merchant <merch.kk12@gmail.com>
2025-10-22 19:16:43 -0400

    Week 3 backend-frotnend connection code

--- api/main.py
+++ api/main.py
@@ -5,19 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 import random
 
 # Initialize the FastAPI application
-
-FOOD_CHOICES = [
-    "Pizza 🍕",
-    "Tacos 🌮",
-    "Sushi 🍣",
-    "Classic Burger 🍔",
-    "Thai Curry 🌶️",    
-    "Grilled Cheese & Tomato 🍅",
-    "Chicken Shawarma 🐔",
-    "Vegan Bowl 🥗",
-    "Pho Noodle Soup 🍲"
-]
-
 app = FastAPI(
     title="FastAPI Example",
     description="This is an example of using FastAPI"
@@ -32,6 +19,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+data = [
+        {"name": "Alice", "age": 30, "city": "New York"},
+        {"name": "Bob", "age": 24, "city": "London"},
+        {"name": "Charlie", "age": 35, "city": "Paris"},
+    ]
+
 # Define endpoints or routes(PATH)
 @app.get('/') # a "/" on its own is the root directory
 def default_route():
@@ -40,30 +33,9 @@ def default_route():
     """
     return "You have reached the default route. Back-end server is listening..."
 
-@app.get("/example")  
-def get_example():    
+@app.get("/list")  
+def get_list():    
     """
-    This endpoint returns a JSON object consisting of a simple message.
+    This endpoint returns a list of JSON objects.
     """
-    return {"message": "Hello World!", "year":2025}
-
-@app.get("/example2")  
-def get_example2(name):    # can also pass in parameters
-    """
-    This endpoint takes in a parameter called "name"  
-    """ # The frontend will communicate with the backend with the name parameter to present a name
-    return {"message": f"Hello {name}!"} # Remember to indent the return
-    # f before the quotation enables the embedding of a variable inside the string
-
-    # When a user sends a GET request to '/random-choice', this function runs.
-
-@app.get("/random-choice")
-def get_random_food_choice():
-    """
-    Returns a single random food choice from the FOOD_CHOICES list.
-    """
-    # Use the built-in Python 'random' module to pick one item.
-    selected_choice = random.choice(FOOD_CHOICES)
-    
-    # Return a Python dictionary, which FastAPI converts to a JSON response.
-    return {"status": "success", "choice": selected_choice}
+    return data
